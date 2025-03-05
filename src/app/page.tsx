eeadb148d3df1f03467d8fe734daafba1cdcf6f1
HimawanRakha <himawanrakha11@gmail.com>
2025-03-06 02:28:44 +0700

    update review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "@/components/hero/hero";
 import Plus from "@/components/plus/Plus";
+import Review from "@/components/review/review";
+import ReviewChild from "@/components/reviewchild/reviewchild";
 
 export default function Home() {
   return (
     <div className="bg-[#FFF9FA] ">
       <Hero />
       <Plus />
+      <Review />
+      <ReviewChild />
     </div>
   );
 }
---
+++ src/components/reviewchild/reviewchild.tsx
@@ -0,0 +1,28 @@
+import { CircleUser } from "lucide-react";
+import { Star } from "lucide-react";
+export default function reviewchild() {
+  return (
+    <div>
+      <div className="container mx-auto flex flex-col justify-center items-center mt-30 pb-32">
+        <div className="relative max-w-4xl w-full">
+          <h1 className="text-xl font-extrabold text-center">"SUPER SUPER BIG INNOVATION AND AWESOME PRODUCT"</h1>
+
+          <div className="absolute -bottom-14 w-full flex justify-between items-center mt-16">
+            <div className="flex items-center gap-4">
+              <CircleUser size={32} />
+              <h1 className="text-xl font-medium">himawanrkh***@gmail.com</h1>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <Star size={32} fill="#FFD700" />
+              <Star size={32} fill="#FFD700" />
+              <Star size={32} fill="#FFD700" />
+              <Star size={32} fill="#FFD700" />
+              <Star size={32} fill="#FFD700" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
